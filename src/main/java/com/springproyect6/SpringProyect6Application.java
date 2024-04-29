fc5d8e505eb580daf6eba6bb1d551140fb025c6e
David Cazalla <ncedcb@outlook.es>
2024-04-29 11:00:01 +0200

    creacion de proyecto spring

---
+++ src/main/java/com/springproyect6/SpringProyect6Application.java
@@ -0,0 +1,13 @@
+package com.springproyect6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyect6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyect6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springproyect6/SpringProyect6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproyect6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProyect6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
