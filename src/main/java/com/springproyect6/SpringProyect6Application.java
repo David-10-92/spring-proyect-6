8209eeb883f3fc48082ab9da81bfc73b96781da4
David Cazalla <ncedcb@outlook.es>
2024-05-06 19:50:39 +0200

    importacion de los paquetes del proyecto

--- src/main/java/com/springproyect6/SpringProyect6Application.java
+++ src/main/java/com/springproyect6/SpringProyect6Application.java
@@ -1,7 +1,19 @@
 package com.springproyect6;
 
+import com.springproyect6.service.dtos.CharacterDTO;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringProyect6Application {
