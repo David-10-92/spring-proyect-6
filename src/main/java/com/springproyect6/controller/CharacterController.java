a371324a38f78b57f09017d31f1ae24a7150a6e8
David Cazalla <ncedcb@outlook.es>
2024-05-06 19:50:24 +0200

    controlador

---
+++ src/main/java/com/springproyect6/controller/CharacterController.java
@@ -0,0 +1,60 @@
+package com.springproyect6.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.springproyect6.service.dtos.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.springproyect6.service.RickAndMortyApiService;
+import com.springproyect6.service.errors.ServiceError;
+
+@Controller
+public class CharacterController {
+
+    private <T> ResponseEntity handleRequest(Supplier<T> supplier){
+        try{
+            return ResponseEntity.ok(supplier.get());
+        }catch(ServiceError e){
+            return new ResponseEntity(e.getMessage(), HttpStatusCode.valueOf(e.getErrorCode().getHttpErrorCode()));
+        }
+    }
+
+    @Autowired
+    private RickAndMortyApiService rickAndMortyApiService;
+
+    @GetMapping("/search")
+    public String searchHtml(Model model) {
+        model.addAttribute("searchFormDTO",new SearchFormDTO());
+        return "search";
+    }
+
+    @PostMapping(path ="/search",consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public String searchCharacters(@ModelAttribute SearchFormDTO searchFormDTO, Model model,@RequestParam(defaultValue = "0") int page) throws UnsupportedEncodingException {
+        Page<CharacterDTO> characterPage = rickAndMortyApiService.fetchCharacterPage(PageRequest.of(page,searchFormDTO.getPageSize()), searchFormDTO);
+        model.addAttribute("results",characterPage.getContent());
+        model.addAttribute("totalPages", characterPage.getTotalPages());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalElements", characterPage.getTotalElements());
+        return "characters";
+    }
+
+    @GetMapping("/view-character")
+    public String verFicha(@RequestParam("url") String url, Model model) {
+        CharacterIdDTO characterIdDTO = rickAndMortyApiService.getCharacterDetails(url);
+        model.addAttribute("character", characterIdDTO);
+        return "character-profile";
+    }
+}
