70ad67373ad0e3bd793528cf454b292b2c9ba2c6
David Cazalla <ncedcb@outlook.es>
2024-05-14 12:36:45 +0200

    eliminando import no que no sirven

--- src/main/java/com/springproyect6/persistence/entity/Comment.java
+++ src/main/java/com/springproyect6/persistence/entity/Comment.java
@@ -4,10 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.IdGeneratorType;
-import org.hibernate.annotations.ValueGenerationType;
-
-import java.sql.Timestamp;
 
 @Getter
 @Setter
